06eb696bdf1b9cf3d04476f0ecb0e859613db824
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-02-21 17:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
-  // your code here
+  //SGN your code here
+	var sc = "";
+	if(s1.length==0 || s2.length==0){
+		sc = "";
+	}else{
+		sc = s1[0] + s2[0];
+	}
+	return sc;
+
+	
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
